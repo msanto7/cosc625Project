cc72cc6ffea97cb512e7705859370f9cfa025009
root <mike.santoro42@gmail.com>
2019-12-10 20:37:43 -0800

    env variable

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ from send_mail import send_mail
 app = Flask(__name__)
 
 # Set up the database based on which environment we are running the application 
-ENV = 'dev'
+ENV = 'prod'
 if ENV == 'dev':
     app.debug = True
     app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:root@localhost/group4'
