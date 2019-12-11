715fa940ebbf61234230ef51d86718783c5a7529
root <mike.santoro42@gmail.com>
2019-12-10 20:20:02 -0800

    updating db models and eliminating static logo

--- app.py
+++ app.py
@@ -18,22 +18,6 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 # object we will use to query the db
 db = SQLAlchemy(app)
 
-# create db model
-class Feedback(db.Model):
-    __tablename__ = 'feedback'
-    id = db.Column(db.Integer, primary_key=True)
-    customer = db.Column(db.String(200), unique=True)
-    dealer = db.Column(db.String(200))
-    rating = db.Column(db.Integer)
-    comments = db.Column(db.Text())
-
-    # constructor for db model
-    def __init__(self, customer, dealer, rating, comments):
-        self.customer = customer
-        self.dealer = dealer
-        self.rating = rating
-        self.comments = comments
-
 # db model for user
 class User(db.Model):
     __tablename__ = 'user'
